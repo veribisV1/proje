2fb0d918b31654a7290a0bbc421f0bf9f1e8d980
cantek41 <cantekin41@gmail.com>
2015-02-15 14:00:53 +0200

    procedure insert ve update yapacak metot

--- VeribisTasarım/DBTOOL.cs
+++ VeribisTasarım/DBTOOL.cs
@@ -8,6 +8,11 @@ using System.Web;
 
 namespace VeribisTasarım
 {
+    /// <summary>
+    /// hertürlü DB ye erişim için 
+    /// gerekli olan metotları içeriri
+    /// connectionu=webConfig ten alır
+    /// </summary>
     public class DBTOOL
     {
         SqlConnection connection;
@@ -41,6 +46,26 @@ namespace VeribisTasarım
             DataTable tablo=new DataTable();
             adapter.Fill(tablo);
             return tablo;
+        }
+        /// <summary>
+        /// prosedür ile set etme geriye 
+        /// primarykey olan alan int olarak donecek
+        /// </summary>
+        /// <param name="prosedurAdi">pCompanuInsert</param>
+        /// <param name="parametreler"><@comapanyCode,11></param>
+        /// <returns></returns>
+        public int set(string prosedurAdi,Dictionary<string,string> parametreler)
+        {            
+            komut = new SqlCommand();
+            komut.Connection = connection;
+            komut.CommandType = System.Data.CommandType.StoredProcedure;
+            komut.CommandText = prosedurAdi;
+            foreach (KeyValuePair<string,string> item in parametreler)
+            {
+                komut.Parameters.Add(item.Key, item.Value);
+            }
+            komut.ExecuteNonQuery();   
+            return Convert.ToInt16(komut.Parameters["ReturnValue"].Value); // prosedurden gelen veri int türüne dönüşüp gönderilir
         }
 
     }

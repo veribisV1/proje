85764aa7dc42bc8abe17dc1c7a8ccd773ca14850
cantek41 <cantek41@gmail.com>
2015-03-11 12:54:24 +0200

    login has sha olarak kontrol ediliyor artık

--- VeribisTasarım/Controller/DB_ELEMAN_GETIR.cs
+++ VeribisTasarım/Controller/DB_ELEMAN_GETIR.cs
@@ -61,12 +61,13 @@ namespace VeribisTasarım.Controller
         //kullanıcı giriş kontrol
         public bool validateUser(string username, string password)
         {
+            login.Class1 passwordCodec = new login.Class1();
             StringBuilder sorgu = new StringBuilder();
             sorgu.Append("SELECT USER_CODE as col1, (AUSER_NAME+' '+SURNAME) AS col2 FROM USERS WHERE USER_NAME='");
             sorgu.Append(username);
             sorgu.Append("'");
             sorgu.Append(" AND USER_PASSWORD='");
-            sorgu.Append(password);
+            sorgu.Append(passwordCodec.Encrypt(password));
             sorgu.Append("'");
 
             Dictionary<string, string> liste = db.getEleman(sorgu.ToString());
@@ -79,12 +80,15 @@ namespace VeribisTasarım.Controller
 
         public int getUserID(string username, string password)
         {
+
+
+            login.Class1 passwordCodec = new login.Class1();
             StringBuilder sorgu = new StringBuilder();
-            sorgu.Append("SELECT USER_CODE FROM USERS WHERE USER_NAME='");
+            sorgu.Append("SELECT USER_CODE as col1, (AUSER_NAME+' '+SURNAME) AS col2 FROM USERS WHERE USER_NAME='");
             sorgu.Append(username);
             sorgu.Append("'");
             sorgu.Append(" AND USER_PASSWORD='");
-            sorgu.Append(password);
+            sorgu.Append(passwordCodec.Encrypt(password));
             sorgu.Append("'");
             Dictionary<string, string> liste = db.getEleman(sorgu.ToString());
             return Convert.ToInt32(liste.Values.ToList()[0]);

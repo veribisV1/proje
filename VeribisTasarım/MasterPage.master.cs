4c9c5ceb4c2194759cfa8a60eae592461c607e41
cantek41 <cantek41@gmail.com>
2015-03-14 11:37:12 +0200

    masterpage repeater db baglantilari duzenlendi

--- VeribisTasarım/MasterPage.master.cs
+++ VeribisTasarım/MasterPage.master.cs
@@ -9,6 +9,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Configuration;
 using System.Web.UI.HtmlControls;
+using VeribisTasarım.Controller;
 
 public partial class MasterPage : System.Web.UI.MasterPage
 {
@@ -24,48 +25,38 @@ public partial class MasterPage : System.Web.UI.MasterPage
 
     private void VeriGetir()
     {
-        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["veribis"].ConnectionString);
-        conn.Open();
-        string MenuSql = "select MENU_NAME,SQL from MENULOAD inner join PSQL on MENULOAD.LINK=PSQL.SQL_ID WHERE USER_CODE = @KullaniciTipiId AND MENULOAD.TYPE=2 ";
-        SqlCommand KullaniciMenu = new SqlCommand(MenuSql, conn);
-        KullaniciMenu.Parameters.Add("@KullaniciTipiId", SqlDbType.Int).Value = Convert.ToInt32(Session["KullaniciTipiId"]);
-        SqlDataAdapter dv = new SqlDataAdapter(KullaniciMenu);
-
-        DataTable dtv = new DataTable();
-        dtv.Columns.Add("MENU_NAME");
-        dtv.Columns.Add("SQL");
-        dv.Fill(dtv);
+        DBARACISI adapter = new DBARACISI();
         Repeater Repeater2 = (Repeater)this.FindControl("Repeater2");
-
-        Repeater2.DataSource = dtv;
-        Repeater2.DataBind();
-
-
-        conn.Close();
+        Repeater2.DataSource = adapter.getGridIcerik(String.Format("select MENU_NAME,SQL from MENULOAD inner join PSQL on MENULOAD.LINK=PSQL.SQL_ID WHERE USER_CODE = {0} AND MENULOAD.TYPE=2", Session["USER_CODE"]));
+        Repeater2.DataBind();      
 
     }
 
 
     private void MenuGoster()
     {
-        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["veribis"].ConnectionString);
-        conn.Open();
-        string MenuSql = "SELECT MENU_NAME,LINK FROM MENULOAD WITH (NOLOCK) WHERE USER_CODE = @KullaniciTipiId AND TYPE=1 ORDER BY ORDER_BY";
-        SqlCommand KullaniciMenu = new SqlCommand(MenuSql, conn);
-        KullaniciMenu.Parameters.Add("@KullaniciTipiId", SqlDbType.Int).Value = Convert.ToInt32(Session["KullaniciTipiId"]);
-        SqlDataAdapter dv = new SqlDataAdapter(KullaniciMenu);
-
-        DataTable dtv = new DataTable();
-        dtv.Columns.Add("MENU_NAME");
-        dtv.Columns.Add("LINK");
-        dv.Fill(dtv);
+        DBARACISI adapter = new DBARACISI();
         Repeater Repeater1 = (Repeater)this.FindControl("Repeater1");
+        Repeater1.DataSource = adapter.getGridIcerik(String.Format("SELECT MENU_NAME,LINK FROM MENULOAD WITH (NOLOCK) WHERE USER_CODE = {0} AND TYPE=1 ORDER BY ORDER_BY", Session["USER_CODE"]));
+        Repeater1.DataBind();    
+        //SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["veribis"].ConnectionString);
+        //conn.Open();
+        //string MenuSql = "SELECT MENU_NAME,LINK FROM MENULOAD WITH (NOLOCK) WHERE USER_CODE = @KullaniciTipiId AND TYPE=1 ORDER BY ORDER_BY";
+        //SqlCommand KullaniciMenu = new SqlCommand(MenuSql, conn);
+        //KullaniciMenu.Parameters.Add("@KullaniciTipiId", SqlDbType.Int).Value = Convert.ToInt32(Session["KullaniciTipiId"]);
+        //SqlDataAdapter dv = new SqlDataAdapter(KullaniciMenu);
 
-        Repeater1.DataSource = dtv;
-        Repeater1.DataBind();
+        //DataTable dtv = new DataTable();
+        //dtv.Columns.Add("MENU_NAME");
+        //dtv.Columns.Add("LINK");
+        //dv.Fill(dtv);
+        //Repeater Repeater1 = (Repeater)this.FindControl("Repeater1");
+
+        //Repeater1.DataSource = dtv;
+        //Repeater1.DataBind();
 
 
-        conn.Close();
+        //conn.Close();
 
     }
 

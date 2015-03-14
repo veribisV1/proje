c1d4dad0506dad326b6b6060b50d5e87fd4850ea
cantek41 <cantek41@gmail.com>
2015-03-14 11:38:01 +0200

    masterpage repeater db baglantilari duzenlendi

--- VeribisTasarım/MasterPage.master.cs
+++ VeribisTasarım/MasterPage.master.cs
@@ -29,7 +29,6 @@ public partial class MasterPage : System.Web.UI.MasterPage
         Repeater Repeater2 = (Repeater)this.FindControl("Repeater2");
         Repeater2.DataSource = adapter.getGridIcerik(String.Format("select MENU_NAME,SQL from MENULOAD inner join PSQL on MENULOAD.LINK=PSQL.SQL_ID WHERE USER_CODE = {0} AND MENULOAD.TYPE=2", Session["USER_CODE"]));
         Repeater2.DataBind();      
-
     }
 
 
@@ -38,25 +37,7 @@ public partial class MasterPage : System.Web.UI.MasterPage
         DBARACISI adapter = new DBARACISI();
         Repeater Repeater1 = (Repeater)this.FindControl("Repeater1");
         Repeater1.DataSource = adapter.getGridIcerik(String.Format("SELECT MENU_NAME,LINK FROM MENULOAD WITH (NOLOCK) WHERE USER_CODE = {0} AND TYPE=1 ORDER BY ORDER_BY", Session["USER_CODE"]));
-        Repeater1.DataBind();    
-        //SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["veribis"].ConnectionString);
-        //conn.Open();
-        //string MenuSql = "SELECT MENU_NAME,LINK FROM MENULOAD WITH (NOLOCK) WHERE USER_CODE = @KullaniciTipiId AND TYPE=1 ORDER BY ORDER_BY";
-        //SqlCommand KullaniciMenu = new SqlCommand(MenuSql, conn);
-        //KullaniciMenu.Parameters.Add("@KullaniciTipiId", SqlDbType.Int).Value = Convert.ToInt32(Session["KullaniciTipiId"]);
-        //SqlDataAdapter dv = new SqlDataAdapter(KullaniciMenu);
-
-        //DataTable dtv = new DataTable();
-        //dtv.Columns.Add("MENU_NAME");
-        //dtv.Columns.Add("LINK");
-        //dv.Fill(dtv);
-        //Repeater Repeater1 = (Repeater)this.FindControl("Repeater1");
-
-        //Repeater1.DataSource = dtv;
-        //Repeater1.DataBind();
-
-
-        //conn.Close();
+        Repeater1.DataBind();           
 
     }
 
